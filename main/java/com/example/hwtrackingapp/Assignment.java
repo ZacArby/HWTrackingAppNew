37e74583ef46e9ca9b7b42907845263028713856
Oscar <oscar.ruiz@ashbury.ca>
2023-04-11 10:26:33 -0400

    dynamically adjust window size depending on screen size

--- main/java/com/example/hwtrackingapp/Assignment.java
+++ main/java/com/example/hwtrackingapp/Assignment.java
@@ -1,17 +1,37 @@
 package com.example.hwtrackingapp;
 
-public class Assignment {
+public class Assignment extends Driver {
+    int curr;
+     int[] daysTillDue = new int[3];
+    int[] dailyWork = new int[3];
 
 
-        public Assignment(String name, String dueDate, int timeRequired, int timeLeft , int daysStreak) {
+        public Assignment(String name, String dueDate, double timeRequired, int dailyWork , int daysTillDue) {
+            Assignment assignment1 = new Assignment(assignmentTitles[0], dueDates[0], totalTimes[0] , this.daysTillDue[0], 1);
+            Assignment assignment2 = new Assignment(assignmentTitles[1], dueDates[1], totalTimes[1] , this.daysTillDue[1] , 1);
+            Assignment assignment3 = new Assignment(assignmentTitles[2], dueDates[2], totalTimes[2] , this.daysTillDue[2] , 1);
+            Assignment assignment4 = new Assignment(assignmentTitles[3], dueDates[3], totalTimes[3] , this.daysTillDue[3] , 1);
+            int dw = 0;
+            for (int i = 0; i < 4; i++) {
+                curr = i;
+                new Countdown();
+
+
+            }
         }
 
-        // getters and setters for name, dueDate, and timeRequired
+    public Assignment() {
 
-        public void main(String[] args) {
-            Assignment assignment1 = new Assignment("Math Homework", "2022-10-15", 120 , 1 , 1);
-            Assignment assignment2 = new Assignment("English Essay", "2022-10-20", 90 , 1 , 1);
-            Assignment assignment3 = new Assignment("Science Project", "2022-11-01", 180 , 1 , 1);
+    }
+
+
+    // getters and setters for name, dueDate, and timeRequired
+
+        public static void main(String[] args) {
+            new Assignment();
+
+
+
 
 
         }
--- main/java/com/example/hwtrackingapp/Countdown.java
+++ main/java/com/example/hwtrackingapp/Countdown.java
@@ -1,26 +1,57 @@
 package com.example.hwtrackingapp;
 
 import java.text.ParseException;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.time.*;
-import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
-public class Countdown {
+public class Countdown  extends Assignment{
     int daysUntil;
+
+
+
 
     public static void main(String[] args) {
 
 
 
+
     }
+    public class streak {
+        int streak;
+        int days;
+        LocalDate lastDay;
+        LocalDate currDate = java.time.LocalDate.now();
+        public static void main(String[] args) {
+
+
+        }
+
+        public void days(String[] args) {
+            if (lastDay == null) {
+                lastDay = java.time.LocalDate.now();
+                streak = 0;
+
+            } else if (lastDay == LocalDate.EPOCH.minusDays(1) ) {
+                lastDay = currDate;
+                streak = streak + 1;
+
+
+            } else  {
+                lastDay = currDate;
+                streak = 0;
+
+            }
+
+        }
+    }
+
     public void counter() throws ParseException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter due date (yyyy-mm-dd):");
-        String dueDate;
-        dueDate = scanner.nextLine();
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter due date (yyyy-mm-dd):");
+        String dueDate = assignmentTitles[curr];
+       // dueDate = scanner.nextLine();
         // System.out.println(dueDate);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Date currDate = format.parse(String.valueOf(LocalDate.now()));
@@ -35,5 +66,39 @@ public class Countdown {
         System.out.println("Days: " + daysTo);
         daysUntil = (int) daysTo;
 
+        daysTillDue[curr] = daysUntil;
+
+    }
+    public class timeCalc {
+        LocalDate dueDate;
+        double totalWork;
+
+
+        public static void main(String[] args) {
+
+        }
+
+
+        public void setDailyWork() {
+            for (int i = 0; i < 4; i++) {
+                curr = i;
+                new Countdown();
+                new timeCalc();
+                new streak();
+                new Assignment();
+                try {
+                    counter();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                daysUntil = daysTillDue[curr];
+                totalWork =  totalTimes[curr];
+                dailyWork[curr] = (int) (totalWork / daysUntil);
+
+
+            }
+
+
+        }
     }
 }
